7e9d9b7a25635538174151c38a834fac2e26fa99
Alsotang <alsotang@gmail.com>
2013-05-11 21:05:01 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SdtExpress::Application.config.secret_token = '0216ac76aa74003dee4533b804e8ab2841f526567615f34c97faf44da2e9e28f6a5be4d25eaab2dfcb22aa397c5ab16906860cafa6dafe6351441b039534685d'
